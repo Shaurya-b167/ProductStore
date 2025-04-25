622445fbbf323b9027486438c891535b53c2801e
Shaurya Beriwala <shauryaberiwala@shauryas-laptop.mgmt.clients.wireless.pfw.edu>
2025-04-25 17:17:52 -0400

    Orders Management

--- frontend/src/pages/Orders/PlaceOrder.jsx
+++ frontend/src/pages/Orders/PlaceOrder.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../../components/Message";
-import ProgressSteps from "../../components/ProgressSteps";
+import ProgressSteps from "../../components/progressSteps";
 import Loader from "../../components/Loader";
 import { useCreateOrderMutation } from "../../redux/api/orderApISlice";
 import { clearCartItems } from "../../redux/features/cart/cartSlice";
