c693a3382417c3e235186ab9d54ce5db3774057b
Shaurya Beriwala <shauryaberiwala@shauryas-laptop.mgmt.clients.wireless.pfw.edu>
2025-04-25 17:35:08 -0400

    Frontend Dev Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      Hello World
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
